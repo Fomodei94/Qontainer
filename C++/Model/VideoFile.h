87374a9027044b532631b9de39fdeb703dec4f98
Federico <federico.omodei@gmail.com>
2018-12-08 16:29:34 +0100

    Inizio definizione classe Date

--- C++/Model/VideoFile.h
+++ C++/Model/VideoFile.h
@@ -8,6 +8,24 @@ class VideoFile {
 		Date publishingDate;
 		string nation;
 		
+		class Date {
+			private:
+				int day;
+				int month;
+				int year;
+			public:
+				//GETTERS:
+				int getDay();
+				int getMonth();
+				int getYear();
+				//SETTERS:
+				void setDay(int d);
+				void setMonth(int m);
+				void setYear(int y);
+				//CONSISTENCE CHECK:
+				bool isDateCorrect(int d, int m, int y);
+		};
+		
 	public:
 		// GETTERS:
 		string getTitle();
