b5266e7b915458dc3cad81c830783b5d8eb7e01c
Federico <federico.omodei@gmail.com>
2019-02-07 18:18:47 +0100

    first test pass for Qontainer template class

--- C++/Model/Tests/test_Qontainer.cpp
+++ C++/Model/Tests/test_Qontainer.cpp
@@ -1,7 +1,14 @@
 #include "../Qontainer.h"
 #include "../VideoFile.h"
+#include <iostream>
 
 int main() {
 	Qontainer<VideoFile> myQontainer;
+	VideoFile video1, video2, video3;
 	
+	std::cout<<myQontainer.isEmpty()<<"   "<<myQontainer.isFull()<<std::endl;
+	myQontainer.pushBack(video1);
+	std::cout<<myQontainer.isEmpty()<<"   "<<myQontainer.isFull()<<std::endl;
+	myQontainer.pushBack(video2);
+	std::cout<<myQontainer.isEmpty()<<"   "<<myQontainer.isFull()<<std::endl;
 }
