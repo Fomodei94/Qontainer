63441ac2a6738b5b4360d13660f1ea9c2813b362
Federico <federico.omodei@gmail.com>
2019-02-09 17:56:27 +0100

    merge Qontainer.h from testing branch

--- C++/Model/Qontainer.h
+++ C++/Model/Qontainer.h
@@ -5,10 +5,11 @@ template <class any_type>
 
 class Qontainer {
 	private:
+			
 		// Private data fields:
 		any_type* collection;
-		int arraySize;
-		int obj_count;
+		unsigned int arraySize;
+		unsigned int obj_count;
 		
 		// Private Methods:
 		void resize(int new_size) {
@@ -16,24 +17,60 @@ class Qontainer {
 			new_array = new any_type[new_size];
 			for(int i=0; i<arraySize; ++i) {
 				new_array[i] = collection[i];
-				delete collection[i];
 			}
+			delete[] collection;
 			collection = new_array;
 			arraySize = new_size;
 		}
 		
+		void Shift(int index) {
+			it = collection + index;
+			for(; it<arraySize-1; ++it) {
+				*it = *(it+1);
+			}
+		}
+		
 	public:
 		// Constructors:
-		Qontainer(int s=2) : collection(new any_type[s]), arraySize(s), obj_count(0) {
+		Qontainer(unsigned int s=2) : collection(new any_type[s]), arraySize(s), obj_count(0) {
 
 		}
 		
 		// Destructor:
-		~Qontainer()=default;
+		~Qontainer() {
+			delete[] collection;
+		}
 		
 		// Getters:
 		int getSize() const {
 			return arraySize;
+		}
+		
+		int getObjCount() const {
+			return obj_count;
+		}
+		
+		// Iterator:
+		typedef any_type* iterator;
+		
+		iterator begin() {
+			return collection;
+		}
+		
+		iterator end() {
+			return collection + obj_count;
+		}
+		
+		// Operators:
+		any_type& operator[] (int i) {
+			if(i>=0 && i<obj_count) {
+				return collection[i];
+			}
+			else throw("Out of bound element request");
+		}
+		
+		Qontainer& operator->() {
+			return (*this);
 		}
 		
 		// Public Methods:
@@ -48,19 +85,21 @@ class Qontainer {
 			else return false;
 		}
 		
-		void pushBack(any_type new_element) {
-			if(isFull()) resize(size + 10);
+		bool pushBack(any_type new_element) {
+			if(isFull()) resize(arraySize + 2);
 			
 			if(!isFull()){
 				collection[obj_count] = new_element;
 				obj_count++;
+				return true;
 			}
 			
-			else throw("Error with element insertion!");
-			
+			else return false; // Insertion Failed.
 		}
 		
-		//void remove(any_type t);
+		void remove(const any_type& t) {	// Removes items of type t.
+			
+		}
 		
 };
 
