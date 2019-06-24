832a2187f457f77eefdcbd5cfd5d8bd4c09e918a
Federico <federico.omodei@gmail.com>
2019-06-24 02:00:59 +0200

    Add autofill of container from JSON when application starts

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,10 +1,12 @@
 #include "View/MainWindow.h"
 #include <QApplication>
+#include "LoadSave.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Qontainer<VideoFile*> *container = new Qontainer<VideoFile*>();
+    loadFromFile(container, QString("./savedLibrary.json"));
     MainWindow w(container);
     w.show();
 
